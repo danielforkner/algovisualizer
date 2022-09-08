2c7acfe28b8d3e69b6d6100a6691e91272f26b25
danielforkner <daniel.forkner@gmail.com>
2022-09-08 12:35:08 -0400

    merge sort chart added

--- src/features/sorting/MergeSort.jsx
+++ src/features/sorting/MergeSort.jsx
@@ -3,21 +3,45 @@ import { useSelector } from 'react-redux';
 import { wait } from './helpers';
 import './styles/mergeSort.css';
 
-const MergeSort = ({ speed }) => {
+const MergeSort = ({ speed, Chart }) => {
+  // component state
   const [grid, setGrid] = useState([]);
   const [startTime, setStartTime] = useState(0);
   const [endTime, setEndTime] = useState(0);
   const [waitCount, setWaitCount] = useState(0);
+  // store
   const select = (idx) => document.getElementById(`mergesort:${idx}`);
   const mainGrid = useSelector((state) => state.sorting.grid);
   const sorting = useSelector((state) => state.sorting.sorting);
+  // chart
+  const [c, setC] = useState(null);
+  const [ctx, setCtx] = useState(null);
+  const [currentChart, setCurrentChart] = useState(null);
+  const [backgroundColors, setBackgroundcolors] = useState([]);
+  const barColor = 'rgba(201, 203, 207, 1)';
+  const sortedColor = 'rgba(140, 140, 125, 1)';
+  const completeColor = 'rgba(140, 255, 125, 1)';
 
+  // get canvas from DOM
+  useEffect(() => {
+    setC(document.getElementById('mergeChart'));
+    if (c) {
+      setCtx(c.getContext('2d'));
+    }
+  }, [c, ctx]);
+
+  // refresh component state
   useEffect(() => {
     const buildGrid = async () => {
+      setBackgroundcolors([]);
       let array = [];
       if (select(0)) select(0).className = 'cell'; // without this the first cell retains the classnames(?)
       for (let i = 0; i < mainGrid.length; i++) {
         array.push(mainGrid[i]);
+        setBackgroundcolors((backgroundColors) => [
+          ...backgroundColors,
+          barColor,
+        ]);
         setGrid([...array]);
         await wait(40);
       }
@@ -30,18 +54,65 @@ const MergeSort = ({ speed }) => {
     if (sorting) sort();
   }, [sorting]);
 
+  // create chart
+  useEffect(() => {
+    const data = {
+      labels: [...grid.keys()],
+      datasets: [
+        {
+          data: grid,
+          backgroundColor: backgroundColors,
+          labels: false,
+        },
+      ],
+    };
+    const config = {
+      type: 'bar',
+      data,
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    };
+    const buildChart = () => {
+      if (currentChart) currentChart.destroy();
+      const chart = new Chart(ctx, config);
+      chart.options.animation = false; // disables all animations
+      setCurrentChart(chart);
+    };
+
+    if (ctx && !currentChart) buildChart();
+    if (currentChart) {
+      currentChart.config.data = data;
+      currentChart.update();
+    }
+  }, [grid, ctx, backgroundColors]);
+
+  const updateChartColors = (array, color, idx) => {
+    let replace = array.indexOf(color);
+    const newArray = [...array];
+    if (replace >= 0) newArray[replace] = barColor;
+    newArray[idx] = color;
+    return newArray;
+  };
+
   async function mergeSort(array, start, end) {
     if (start < end) {
       let mid = Math.floor((start + end) / 2);
 
       await mergeSort(array, start, mid);
       await mergeSort(array, mid + 1, end);
-      await merge(array, start, mid, end);
+      merge(array, start, mid, end);
+      await wait(speed);
+      setWaitCount((waitCount) => (waitCount += 1));
     }
     return array;
   }
 
-  async function merge(array, start, mid, end) {
+  function merge(array, start, mid, end) {
     let leftLen = mid - start + 1;
     let rightLen = end - mid;
 
@@ -60,49 +131,71 @@ const MergeSort = ({ speed }) => {
     let pointer = start;
 
     while (leftIdx < leftLen && rightIdx < rightLen) {
+      let idx = pointer;
       if (leftArr[leftIdx] <= rightArr[rightIdx]) {
         array[pointer] = leftArr[leftIdx++];
-        select(pointer).classList.remove('left', 'right');
-        select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
+        setBackgroundcolors((backgroundColors) => {
+          const newArray = [...backgroundColors];
+          newArray[idx] = sortedColor;
+          return newArray;
+        });
+        // select(pointer).classList.remove('left', 'right');
+        // select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
       } else {
         array[pointer] = rightArr[rightIdx++];
-        select(pointer).classList.remove('left', 'right');
-        select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
+        setBackgroundcolors((backgroundColors) => {
+          const newArray = [...backgroundColors];
+          newArray[idx] = sortedColor;
+          return newArray;
+        });
+        // select(pointer).classList.remove('left', 'right');
+        // select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
       }
       pointer++;
     }
 
     while (leftIdx < leftLen) {
-      select(pointer).classList.remove('left', 'right');
-      select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
+      let idx = pointer;
+      // select(pointer).classList.remove('left', 'right');
+      // select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
+      setBackgroundcolors((backgroundColors) => {
+        const newArray = [...backgroundColors];
+        newArray[idx] = sortedColor;
+        return newArray;
+      });
       array[pointer++] = leftArr[leftIdx++];
     }
     while (rightIdx < rightLen) {
-      select(pointer).classList.add('left', 'right');
-      select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
+      let idx = pointer;
+      // select(pointer).classList.add('left', 'right');
+      // select(pointer).classList.add(start % 2 === 0 ? 'left' : 'right');
+      setBackgroundcolors((backgroundColors) => {
+        const newArray = [...backgroundColors];
+        newArray[idx] = sortedColor;
+        return newArray;
+      });
       array[pointer++] = rightArr[rightIdx++];
     }
     setGrid([...array]);
-    await wait(speed);
-    setWaitCount((waitCount) => (waitCount += 1));
   }
   async function sort() {
     setStartTime(Date.now());
     setWaitCount(() => 0);
     await mergeSort([...grid], 0, grid.length - 1);
     setEndTime(Date.now());
+    // finished sorting
     for (let i = 0; i < grid.length; i++) {
-      let cell = select(i);
-      cell.classList.remove('left', 'right');
-      cell.classList.add('complete');
-      cell.classList.add('sorted');
+      setBackgroundcolors((backgroundColors) => {
+        return ([...backgroundColors][i] = completeColor);
+      });
     }
   }
 
   return (
     <div>
       <h1>Merge Sort</h1>
-      <div className="grid-container">
+      <canvas id="mergeChart"></canvas>
+      {/* <div className="grid-container">
         {grid.map((elem, idx) => {
           return (
             <div
@@ -114,7 +207,7 @@ const MergeSort = ({ speed }) => {
             </div>
           );
         })}
-      </div>
+      </div> */}
       <div className="end-time">
         {endTime
           ? `Time to sort: ${(
